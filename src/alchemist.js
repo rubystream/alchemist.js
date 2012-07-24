0a255b9440b204231c80d60f358b881584aa7907
Dejan Dimic <ddimic@smithmicro.com>
2012-07-24 10:28:11 +0200

    add alchemist.js file

---
+++ src/alchemist.js
@@ -0,0 +1,20 @@
+//
+//  alchemist.js
+//
+//  alchemist.js is a unit conversion module.
+//
+//
+//  Copyright 2012-2013 Dejan Dimic (dejan.dimic@gmail.com)
+//
+//  Released under the MIT License.
+//
+//  ------------------------------------------------
+//  author: Dejan Dimic
+//  version: 0.0.1
+//  url: http://rubystream.github.com/alchemist.js/
+//  source: http://github.com/rubystream/alchemist.js/
+//
+
+var Alchemist = (function( {
+
+} ))();
